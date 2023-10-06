bf5ed48a62698519d8996e22d15652003539ebab
dcastillohernandez <dayroncastillohernandez1994@gmail.com>
2023-10-06 13:22:00 -0600

    update

---
+++ cap_1/demo_querystring.js
@@ -0,0 +1,16 @@
+//Cuando navegamos a la dirección xhttp://localhost:3000/?year=2017&month=julio, nos mostraría 2017 julio
+
+var http = require("http");
+var url = require("url");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
+  })
+  .listen(3000);
+
+
+  
