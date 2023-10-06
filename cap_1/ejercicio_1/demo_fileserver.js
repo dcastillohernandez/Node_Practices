69bfc1a336f3fc0a54ec96c60d1f6cd547052160
dcastillohernandez <dayroncastillohernandez1994@gmail.com>
2023-10-06 14:31:13 -0600

    Update

--- cap_1/ejercicio_1/demo_fileserver.js
+++ cap_1/ejercicio_1/demo_fileserver.js
@@ -1,7 +1,19 @@
-const express = require('express')
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
 
-const app = express()
-
-app.listen(3000, () => {
-    console.log(`Server is Listening on 3000`)
-});
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+      return res.end();
+    });
+  })
+  .listen(3000);
