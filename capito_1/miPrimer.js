770192060d19875752f6ae5fa0b75beb5c05ab1f
dcastillohernandez <dayroncastillohernandez1994@gmail.com>
2023-10-06 11:48:30 -0600

    update

--- capito_1/miPrimer.js
+++ capito_1/miPrimer.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var dt = require('./modules/myfirstmodule')
+var dt = require('../modules/myfirstmodule')
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
