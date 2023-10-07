bfbdd7f73b07c557532466ffdaaa71b64e4fd995
dcastillohernandez <dayroncastillohernandez1994@gmail.com>
2023-10-07 16:04:31 -0600

    update

--- cap_1/demo_querystring.js
+++ cap_1/demo_querystring.js
@@ -1,4 +1,4 @@
-//Accedemos desde la terminal a la dirección donde tenemos el archivo 
+//Accedemos desde la terminal a la dirección donde tenemos el archivo
 //Luego ejecutamos el comando node + {nombre del archivo con su extención .js}
 //ejemplo: node demo_querystring.js
 //Cuando navegamos a la dirección http://localhost:3000/?year=2017&month=julio, nos mostraría 2017 julio
@@ -8,12 +8,9 @@ var url = require("url");
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { "Content-Type": "text/html" });
     var q = url.parse(req.url, true).query;
     var txt = q.year + " " + q.month;
     res.end(txt);
   })
   .listen(3000);
-
-
-  
--- cap_1/miPrimer.js
+++ cap_1/miPrimer.js
@@ -1,8 +1,10 @@
-var http = require('http');
-var dt = require('../modules/myfirstmodule')
+var http = require("http");
+var dt = require("../modules/myfirstmodule");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
-}).listen(3000);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
+  })
+  .listen(3000);
